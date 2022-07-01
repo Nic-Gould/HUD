2cfeab74900bc3b5226b5303073b9be6eaa6a006
zen <zen@temple>
2022-07-01 13:39:18 +1000

    housekeeping

--- hud.js
+++ hud.js
@@ -1,1 +1,21 @@
 alert("Hello There");
+
+//
+onmouseclick(){
+	case : mode = "create glass";
+		glass.create(mousepointer.x, mousepointer.y)
+	case : mode = "menu";
+		menu.load())
+}
+
+glass.oncreate(x,y){
+		
+		HUD.canvas.drawrect(x,y,x-getmousepointerx, y - getmousepointer.y);
+		style = widget;
+		}
+		
+computer vision keyboard
+
+okay so i guess anything like this will be implemented in python
+web stack can be used for drawing the display i guess. 
+//
